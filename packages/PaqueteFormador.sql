c49eb0019d31f31bc6fd26293cccf38cf3d90696
mafv-NTER <miguel.fuentes.valenzuela@nter.es>
2024-07-29 09:18:28 +0200

    Actualizado paquete Formación

--- packages/PaqueteFormador.sql
+++ packages/PaqueteFormador.sql
@@ -1,7 +1,6 @@
 CREATE OR REPLACE PACKAGE FormacionPkg IS
     -- Registrar Nueva Formación
-    PROCEDURE RegistrarNuevaFormacion(
-        p_id_formacion  IN VARCHAR2,
+    PROCEDURE RegFormacion(
         p_tecnologia    IN VARCHAR2,
         p_departamento  IN VARCHAR2,
         p_fecha_inicio  IN DATE,
@@ -20,8 +19,7 @@ CREATE OR REPLACE PACKAGE FormacionPkg IS
     );
 
     -- Registrar Evaluación
-    PROCEDURE RegistrarEvaluacion(
-        p_id_evaluacion     IN VARCHAR2,
+    PROCEDURE RegEvaluacion(
         p_id_formacion      IN VARCHAR2,
         p_nota              IN NUMBER,
         p_fecha_evaluacion  IN DATE,
@@ -52,8 +50,7 @@ CREATE OR REPLACE PACKAGE FormacionPkg IS
     );
 
     -- Registrar Nueva Tecnología
-    PROCEDURE RegistrarNuevaTecnologia(
-        p_id_tecnologia IN VARCHAR2,
+    PROCEDURE RegTecnologia(
         p_nombre        IN VARCHAR2,
         p_descripcion   IN VARCHAR2
     );
@@ -80,8 +77,17 @@ END FormacionPkg;
 /
 
 CREATE OR REPLACE PACKAGE BODY FormacionPkg IS
-    PROCEDURE RegistrarNuevaFormacion(
-        p_id_formacion  IN VARCHAR2,
+
+    FUNCTION formacion_existe(id_form IN VARCHAR2) RETURN BOOLEAN IS
+        BEGIN
+            FOR i IN (SELECT ID_FORMACION FROM FORMACION WHERE ID_FORMACION = id_form) LOOP
+                RETURN TRUE;
+            END LOOP;
+            
+            RETURN FALSE;
+        END formacion_existe;
+        
+    PROCEDURE RegFormacion(
         p_tecnologia    IN VARCHAR2,
         p_departamento  IN VARCHAR2,
         p_fecha_inicio  IN DATE,
@@ -93,13 +99,14 @@ CREATE OR REPLACE PACKAGE BODY FormacionPkg IS
             RAISE_APPLICATION_ERROR(-21000, 'ERROR DE REGISTRO DE FORMACIÓN! La fecha de inicio no puede ser posterior ó igual a la fecha de Fin');
         END IF;
     
+        --Se usan IDs a número 00000, ya que gracias al Trigger correspondiente se introducirá un ID autogenerado con secuencias propias.
         INSERT INTO Formacion (id_formacion, id_tecnologia, id_departamento, fecha_inicio, fecha_fin, id_empleado)
-        VALUES (p_id_formacion, p_tecnologia, p_departamento, p_fecha_inicio, p_fecha_fin, p_id_formador);
+        VALUES ('FOR-00000', p_tecnologia, p_departamento, p_fecha_inicio, p_fecha_fin, p_id_formador);
         
         EXCEPTION
             WHEN OTHERS THEN
-            DBMS_OUTPUT.PUT_LINE('error al registrar la nueva formación');
-    END RegistrarNuevaFormacion;
+            DBMS_OUTPUT.PUT_LINE('error al registrar la nueva formación indicada');
+    END RegFormacion;
 
     PROCEDURE ActualizarFormacion(
         p_id_formacion  IN VARCHAR2,
@@ -117,10 +124,14 @@ CREATE OR REPLACE PACKAGE BODY FormacionPkg IS
             fecha_fin = p_fecha_fin,
             id_empleado = p_id_formador
         WHERE id_formacion = p_id_formacion;
+        
+        EXCEPTION
+            WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('error al actualizar la formación indicada:');
+            DBMS_OUTPUT.PUT_LINE(TO_CHAR(SQLERRM));
     END ActualizarFormacion;
 
-    PROCEDURE RegistrarEvaluacion(
-        p_id_evaluacion     IN VARCHAR2,
+    PROCEDURE RegEvaluacion(
         p_id_formacion      IN VARCHAR2,
         p_nota              IN NUMBER,
         p_fecha_evaluacion  IN DATE,
@@ -129,8 +140,13 @@ CREATE OR REPLACE PACKAGE BODY FormacionPkg IS
     ) IS
     BEGIN
         INSERT INTO Evaluacion (id_evaluacion, id_formacion, nota, fecha_evaluacion, observaciones, id_empleado)
-        VALUES (p_id_evaluacion, p_id_formacion, p_nota, p_fecha_evaluacion, p_observaciones, p_id_empleado);
-    END RegistrarEvaluacion;
+        VALUES ('EVA-00000', p_id_formacion, p_nota, p_fecha_evaluacion, p_observaciones, p_id_empleado);
+        
+        EXCEPTION
+            WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('error al registrar la nueva evaluación indicada');
+            DBMS_OUTPUT.PUT_LINE(TO_CHAR(SQLERRM));
+    END RegEvaluacion;
 
     PROCEDURE ActualizarEvaluacion(
         p_id_evaluacion     IN VARCHAR2,
@@ -148,6 +164,11 @@ CREATE OR REPLACE PACKAGE BODY FormacionPkg IS
             observaciones = p_observaciones,
             id_empleado = p_id_empleado
         WHERE id_evaluacion = p_id_evaluacion;
+        
+        EXCEPTION
+            WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('error al actualizar la evaluación indicada');
+            DBMS_OUTPUT.PUT_LINE(TO_CHAR(SQLERRM));
     END ActualizarEvaluacion;
 
     PROCEDURE ObtenerEvaluacionesPorFormacion(
@@ -166,17 +187,26 @@ CREATE OR REPLACE PACKAGE BODY FormacionPkg IS
     BEGIN
         OPEN p_resultado FOR
         SELECT * FROM Evaluacion WHERE id_empleado = p_id_empleado;
+        
+        EXCEPTION
+            WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('error al obtener la evaluación para el empleado ' || TO_CHAR(p_id_empleado));
+            DBMS_OUTPUT.PUT_LINE(TO_CHAR(SQLERRM));
     END ObtenerEvaluacionesPorEmpleado;
 
-    PROCEDURE RegistrarNuevaTecnologia(
-        p_id_tecnologia IN VARCHAR2,
+    PROCEDURE RegTecnologia(
         p_nombre        IN VARCHAR2,
         p_descripcion   IN VARCHAR2
     ) IS
     BEGIN
         INSERT INTO Tecnologia (id_tecnologia, nombre, descripcion)
-        VALUES (p_id_tecnologia, p_nombre, p_descripcion);
-    END RegistrarNuevaTecnologia;
+        VALUES ('TEC-00000', p_nombre, p_descripcion);
+        
+        EXCEPTION
+            WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('error al registrar la tecnología indicada');
+            DBMS_OUTPUT.PUT_LINE(TO_CHAR(SQLERRM));
+    END RegTecnologia;
 
     PROCEDURE ActualizarTecnologia(
         p_id_tecnologia IN VARCHAR2,
@@ -188,6 +218,11 @@ CREATE OR REPLACE PACKAGE BODY FormacionPkg IS
         SET nombre = p_nombre,
             descripcion = p_descripcion
         WHERE id_tecnologia = p_id_tecnologia;
+        
+        EXCEPTION
+            WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('error al actualizar la tecnología indicada (' || p_id_tecnologia || ')');
+            DBMS_OUTPUT.PUT_LINE(TO_CHAR(SQLERRM));
     END ActualizarTecnologia;
 
     PROCEDURE ObtenerTecnologiaPorNombre(
@@ -197,6 +232,11 @@ CREATE OR REPLACE PACKAGE BODY FormacionPkg IS
     BEGIN
         OPEN p_resultado FOR
         SELECT * FROM Tecnologia WHERE nombre = p_nombre;
+        
+        EXCEPTION
+            WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('error al seleccionar la tecnología con nombre "' || p_nombre || '"');
+            DBMS_OUTPUT.PUT_LINE(TO_CHAR(SQLERRM));
     END ObtenerTecnologiaPorNombre;
 
     PROCEDURE ObtenerFormacionesPorDepartamento(
@@ -206,8 +246,11 @@ CREATE OR REPLACE PACKAGE BODY FormacionPkg IS
     BEGIN
         OPEN p_resultado FOR
         SELECT * FROM Formacion WHERE id_departamento = p_id_departamento;
+        
+        EXCEPTION
+            WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('error al obtener la formación indicada (' || p_id_departamento || ')');
+            DBMS_OUTPUT.PUT_LINE(TO_CHAR(SQLERRM));
     END ObtenerFormacionesPorDepartamento;
 END FormacionPkg;
 /
-
-EXEC formacionpkg.registrarnuevaformacion('FOR-00001', 'TEC-00001', 'DEP-00001', SYSDATE, SYSDATE, 'EMP-00001');
